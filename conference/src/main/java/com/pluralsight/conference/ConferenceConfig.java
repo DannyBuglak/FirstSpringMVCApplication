ebf6d01c6335cf3627fd47114801194475ec813b
Danny Buglak <dannybuglak4@gmail.com>
2024-06-14 12:20:40 -0400

    Module 6 - REST controller

--- conference/src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ conference/src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -3,6 +3,7 @@ package com.pluralsight.conference;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +15,11 @@ import java.util.Locale;
 
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
